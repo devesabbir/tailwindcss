845d13d6244f5452122398e3c2a38a0a333adb66
devesabbir <heartedsabbirdhrubo@gmail.com>
2023-12-20 20:49:06 +0600

    postcss postcss-cli autoprefixer JIT purge

--- installation/tailwind.config.js
+++ installation/tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
+  mode: 'jit',
+    // These paths are just examples, customize them to match your project structure
+    purge: [
+      './public/**/*.html',
+      './src/**/*.{html,js,jsx,ts,tsx,vue}',
+    ],
   content: ["./src/**/*.{html,js}"],
-  darkMode:'media',
+  darkMode:'class',
   theme: {
     extend: {},
   },
